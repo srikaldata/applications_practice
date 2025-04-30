00ebc14febf2bcf32a061e96058d8d6efcd53ff1
srikal <srikaldata@gmail.com>
2025-04-29 22:30:23 -0400

    basic calc app w gui - dropdown menu in gui and positioning it for letting the user choose the operation to be performed - create

--- basic_calculator/basic_calc_gui.py
+++ basic_calculator/basic_calc_gui.py
@@ -42,3 +42,14 @@ usr_input_num1.grid(row=0, column=1, pady=8)
 usr_input_num2 = tk.Entry(root_gui)
 # position of the 1st operand input element in the gui 
 usr_input_num2.grid(row=1, column=1, pady=8)
+
+# user choosing the operation to be performed
+tk.Label(root, text='Select operation: ').grid(row=2, column=0, pady=8)
+operation_selected = tk.StringVar(root_gui)
+# setting the default operation to be performed
+operation_selected.set('Addition') 
+# listing the set of operations to select from
+operations_list = ['Addition', 'Subtraction', 'Multiplication', 'Division', 'Modulus', 'Power']
+# creating a dropdown menu for letting the user choose the operation
+operation_selected_ddmenu = tk.OptionMenu(root_gui, operation_selected, *operations_list)
+operation_selected_ddmenu.grid(row=2, column=1, pady=8)
