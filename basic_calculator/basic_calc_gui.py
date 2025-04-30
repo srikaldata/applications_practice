8ffedfc6bd94e1a615da3efb2549219736ea7411
srikal <srikaldata@gmail.com>
2025-04-29 22:03:22 -0400

    basic calc app w gui - fns for each operation - create

--- basic_calculator/basic_calc_gui.py
+++ basic_calculator/basic_calc_gui.py
@@ -1,3 +1,27 @@
 # importing packages for gui
 import tkinter as tk
 from tkinter import messagebox
+
+
+# individual operation functions
+def addition(num1, num2):
+    return num1+num2
+
+def subtraction(num1, num2):
+    return num1-num2
+
+def multiplication(num1, num2):
+    return num1*num2
+
+def division(num1, num2):
+    if num2 == 0:
+        raise ValueError('Cannot divide by zero!')
+    else:
+        return num1/num2
+
+def modulus(num1, num2):
+    return num1 % num2
+
+def power(num1, num2):
+    return num1 ** num2
+
