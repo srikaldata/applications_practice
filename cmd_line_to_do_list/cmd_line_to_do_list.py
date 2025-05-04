0adde062b9da908174cbfea8a47860dcb0157e05
srikal <srikaldata@gmail.com>
2025-05-04 17:26:39 -0400

    cmd line to do list - fn to add a task in the task list

--- cmd_line_to_do_list/cmd_line_to_do_list.py
+++ cmd_line_to_do_list/cmd_line_to_do_list.py
@@ -12,3 +12,11 @@ def save_tasks():
         for task_name, task_description, task_status in task_list:
             content = f'{task_name}||{task_description}||{task_status}\n'
             task_file.write(content)
+
+
+# function to add and save the task in a txt file
+def add_task(task_name, task_description):
+
+    task_list.append([task_name, task_description, 'PENDING'])
+    print(f'Task {task_name} is successfully added!')
+    save_tasks()
