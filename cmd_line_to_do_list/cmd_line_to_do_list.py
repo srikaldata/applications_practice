bbffc5f85d42cd25a9e5e49bb97b2224823c8e85
srikal <srikaldata@gmail.com>
2025-05-04 17:25:40 -0400

    cmd line to do list - fn to save the list of tasks in a txt file

--- cmd_line_to_do_list/cmd_line_to_do_list.py
+++ cmd_line_to_do_list/cmd_line_to_do_list.py
@@ -4,3 +4,11 @@ import sys
 # initializing an empty task list
 task_list = []
 
+
+# fn to save the task in a txt file
+def save_tasks():
+    with open('tasks.txt', 'a') as task_file:
+        
+        for task_name, task_description, task_status in task_list:
+            content = f'{task_name}||{task_description}||{task_status}\n'
+            task_file.write(content)
