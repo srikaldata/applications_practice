7c50d24e0ecf029ae5570460171df3db1e65d4d8
srikal <srikaldata@gmail.com>
2025-05-12 22:07:13 -0400

    rand num guess game - generate rand num and max attempts based on difficulty level

--- user_random_num_guess_game/user_random_num_guess_game.py
+++ user_random_num_guess_game/user_random_num_guess_game.py
@@ -8,3 +8,20 @@ MAX_ATTEMPTS = 10
 # fn for welcome message
 def welcome_message():
     print('Welcome to the number guessing game!')
+
+
+# fn to generate a random num based on the difficulty level
+def generate_random_num(user_difficulty_level):
+    if user_difficulty_level == 1:
+        num, max_attempts = random.randint(1, 50), 5
+    
+    elif user_difficulty_level == 2:
+        num, max_attempts = random.randint(1, 100), 7
+    
+    elif user_difficulty_level == 3:
+        num, max_attempts = random.randint(1, 1000), 10
+    
+    # default difficulty
+    else:
+        num, max_attempts = random.randint(1, 100), 7
+    return num, max_attempts
