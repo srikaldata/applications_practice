607f3e6399032c1273d354fee87ece31d63e8558
srikal <srikaldata@gmail.com>
2025-05-12 22:06:22 -0400

    rand num guess game - fn for welcome message

--- user_random_num_guess_game/user_random_num_guess_game.py
+++ user_random_num_guess_game/user_random_num_guess_game.py
@@ -1,3 +1,10 @@
 # importing necessary module
 import random
 import sys
+
+# constant for testing
+MAX_ATTEMPTS = 10
+
+# fn for welcome message
+def welcome_message():
+    print('Welcome to the number guessing game!')
