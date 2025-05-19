0226046c8c48454ad4e7bbc6aa3af49bc84af706
srikal <srikaldata@gmail.com>
2025-05-19 17:43:56 -0400

    pomodoro timer - default constants for timers

--- pomodoro_timer/pomodoro_timer.py
+++ pomodoro_timer/pomodoro_timer.py
@@ -6,3 +6,10 @@ from plyer import notification
 # import requests 
 # ntfy.sh (pub sub)
 
+
+# default settings
+DEFAULT_POMODORO_TIMER_MINS = 25
+DEFAULT_SHORT_BREAK_TIME_MINS = 5
+DEFAULT_LONG_BREAK_TIME_MINS = 5
+DEFAULT_CYCLES_FOR_LONG_BREAK = 3
+
