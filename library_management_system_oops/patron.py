5a16e3c282baa1ac737e6f4aad98d4baf6d32acc
srikal <srikaldata@gmail.com>
2025-05-26 18:03:28 -0400

    library mgmt sys oops - patron class - constructor to create attributes

--- library_management_system_oops/patron.py
+++ library_management_system_oops/patron.py
@@ -4,3 +4,9 @@ from abc import ABC, abstractmethod
 
 # creating a class for patron
 class Patron(ABC):
+    
+    # constructor to create attributes for patron
+    def __init__(self, name, patron_id):
+        self.name = name 
+        self.patron_id = patron_id
+        self.borrowed_books = []
