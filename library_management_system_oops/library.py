c5aad26c691412deb04db8e020dc6460554441c5
srikal <srikaldata@gmail.com>
2025-05-27 23:33:26 -0400

    library mgmt sys oops - library class - creating the constructor method for books and patrons catalog

--- library_management_system_oops/library.py
+++ library_management_system_oops/library.py
@@ -1,3 +1,10 @@
 # importing the classes
 from book import Book
 from patron import Student, Faculty
+
+# aggregate class --> Library is a wrapper for other functions
+class Library:
+    # constructor method to create a book shelf and current patrons list
+    def __init__(self):
+        self.books_shelf = []
+        self.patrons_current = []
