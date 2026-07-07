bf9dda4a69216bf85e12c239f7769bf841bd7541
srikal <srikaldata@gmail.com>
2026-07-07 17:43:53 -0400

    learn to code by solving problems - winning team - test the function with multiple test cases

--- learning_to_code_by_solving_problems/1_winning_team.py
+++ learning_to_code_by_solving_problems/1_winning_team.py
@@ -11,13 +11,13 @@
 
 
 # get the inputs
-app_3 = int(input())
-app_2 = int(input())
-app_1 = int(input())
+app_3 = int(input('apple 3 pointers count:'))
+app_2 = int(input('apple 2 pointers count:'))
+app_1 = int(input('apple 1 pointers count:'))
 
-ban_3 = int(input())
-ban_2 = int(input())
-ban_1 = int(input())
+ban_3 = int(input('banana 3 pointers count:'))
+ban_2 = int(input('banana 2 pointers count:'))
+ban_1 = int(input('banana 1 pointers count:'))
 
 # calculate total pts
 app_total = app_3 * 3 + app_2 * 2 + app_1 * 1
@@ -45,3 +45,9 @@ def find_the_winner(app_3,app_2,app_1,ban_3,ban_2,ban_1):
         return 'B'
     else:
         return 'T'
+
+#  test the fn
+print(find_the_winner('A',app_2,app_1,ban_3,ban_2,ban_1))
+print(find_the_winner(1,1,1,1,1,1))
+print(find_the_winner(1,1,1,1,1,0.6))
+print(find_the_winner(app_3,app_2,app_1,ban_3,ban_2,ban_1))
