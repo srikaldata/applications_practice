8346758c167e01d61265c3e8aa2e1bd56db2b24f
srikal <srikaldata@gmail.com>
2026-07-08 23:32:03 -0400

    learn to code by solving problems - telemarketers - improving the code logic by eliminating list range

--- learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
+++ learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
@@ -9,7 +9,13 @@ num_3 = int(input('digit 3:'))
 num_4 = int(input('digit 4:'))
 
 # conditions to verify
-if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3 in list(range(10)):
+if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3==num_2:
+    print('ignore the call')
+else:
+    print('answer the call')
+
+# improving the code
+if (num_1 ==8 or num_1==9) and (num_4 ==8 or num_4==9) and num_3==num_2:
     print('ignore the call')
 else:
     print('answer the call')
