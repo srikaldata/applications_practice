ce4d9b06a9c23b4c301f7413a1ef6dde97186365
srikal <srikaldata@gmail.com>
2026-07-08 23:41:24 -0400

    learn to code by solving problems - telemarketers - catching and raising errors for multiple digits and negative ints

--- learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
+++ learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
@@ -8,14 +8,24 @@ num_2 = int(input('digit 2:'))
 num_3 = int(input('digit 3:'))
 num_4 = int(input('digit 4:'))
 
-# conditions to verify
-if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3==num_2:
+# conditional logic formation
+if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3 in list(range(10)):
     print('ignore the call')
 else:
     print('answer the call')
 
-# improving the code
+# improving the code logic
 if (num_1 ==8 or num_1==9) and (num_4 ==8 or num_4==9) and num_3==num_2:
     print('ignore the call')
 else:
     print('answer the call')
+
+# catching multiple number and negative integers edge cases
+inputs = [num_1, num_2, num_3, num_4]
+if any(not isinstance(n,int) or n<0 or n>9 for n in inputs):
+    print('Invalid input: nums must be in single digit, non-negative ints')
+else:
+    if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3==num_2:
+        print('ignore the call')
+    else:
+        print('answer the call')
