2911272eb8eb424ad5832e175c6f7cf3faac45cd
srikal <srikaldata@gmail.com>
2026-07-08 23:50:58 -0400

    learn to code by solving problems - telemarketers - wrapping the code logic in a function

--- learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
+++ learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
@@ -23,7 +23,7 @@ else:
 # catching multiple number and negative integers edge cases
 inputs = [num_1, num_2, num_3, num_4]
 if any(not isinstance(n,int) or n<0 or n>9 for n in inputs):
-    print('Invalid input: nums must be in single digit, non-negative ints')
+    print('Invalid input: nums must be in single digit and non-negative ints')
 else:
     if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3==num_2:
         print('ignore the call')
@@ -34,7 +34,7 @@ else:
 def answer_or_ignore_call(num_1, num_2, num_3, num_4):
     inputs = [num_1, num_2, num_3, num_4]
     if any(not isinstance(n,int) or n<0 or n>9 for n in inputs):
-        print('Invalid input: nums must be in single digit, non-negative ints')
+        print('Invalid input: nums must be in single digit and non-negative ints')
     else:
         if num_1 in [8,9] and num_4 in [8,9] and num_3 in list(range(10)) and num_3==num_2:
             print('ignore the call')
@@ -43,4 +43,21 @@ def answer_or_ignore_call(num_1, num_2, num_3, num_4):
 
 # calling the fn
 answer_or_ignore_call(num_1, num_2, num_3, num_4)
+
+
+# testing mutiple test conditions
+print()
+print('testing:')
+answer_or_ignore_call(0,1,2,3)
+answer_or_ignore_call(0,1,2,0)
+answer_or_ignore_call(8,1,2,8)
+print()
+answer_or_ignore_call(-8,1,1,-9)
+answer_or_ignore_call(8,1,10,9)
+answer_or_ignore_call(9,'a','a',8)
+answer_or_ignore_call(9,0.6,0,8)
+print()
+answer_or_ignore_call(8,5,5,9)
+answer_or_ignore_call(9,0,0,9)
+answer_or_ignore_call(9,0,0,8)
 
