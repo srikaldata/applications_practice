db846c85b4b80edb0863b08e7f4137f4955ff866
srikal <srikaldata@gmail.com>
2026-07-08 23:18:24 -0400

    learn to code by solving problems - telemarketers - getting the input of four digits

---
+++ learning_to_code_by_solving_problems/2_telemarketers_four_digits.py
@@ -0,0 +1,9 @@
+# 4 digit ph num
+# 1st digit = 8 or 9
+# 2nd and 3rd digits are the same between 0 and 9
+# 4th digit = 8 or 9
+
+num_1 = int(input('digit 1:'))
+num_2 = int(input('digit 2:'))
+num_3 = int(input('digit 3:'))
+num_4 = int(input('digit 4:'))
