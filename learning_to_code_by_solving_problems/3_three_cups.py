26902d45c02e6fdd74896470eb58a54e0373736e
srikal <srikaldata@gmail.com>
2026-07-09 23:05:32 -0400

    learn to code by solving problems - three cups - getting input and setting the initial position

---
+++ learning_to_code_by_solving_problems/3_three_cups.py
@@ -0,0 +1,9 @@
+# 3 opaque cups
+# A = swap left and middle
+# B = swap middle and right
+# C = swap left and right
+
+# getting the ip and setting the initial values
+swap_order = input()
+position = 1
+
