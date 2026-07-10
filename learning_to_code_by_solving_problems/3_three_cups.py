ed06323ef5aff35b19d37631d2704543b6340b50
srikal <srikaldata@gmail.com>
2026-07-09 23:28:43 -0400

    learn to code by solving problems - three cups - looping through the swap order and changing positions

--- learning_to_code_by_solving_problems/3_three_cups.py
+++ learning_to_code_by_solving_problems/3_three_cups.py
@@ -7,3 +7,20 @@
 swap_order = input()
 position = 1
 
+# looping through the swap order and classifying the outcome
+for swap in swap_order:
+    if position == 1 and swap == 'A':
+        position=2
+    if position == 1 and swap == 'C':
+        position=3
+    if  position == 2 and swap == 'A':
+        position=1
+    if  position == 2 and swap == 'B':
+        position=3
+    if  position == 3 and swap == 'B':
+        position=2
+    if  position == 3 and swap == 'C':
+        position=1
+
+# print the final position
+print(position)
