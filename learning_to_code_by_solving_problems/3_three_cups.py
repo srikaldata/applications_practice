7f0d94fcef105f8a88ba5aea09b4f6963b9f0f04
srikal <srikaldata@gmail.com>
2026-07-09 23:54:12 -0400

    learn to code by solving problems - three cups - function with input validation

--- learning_to_code_by_solving_problems/3_three_cups.py
+++ learning_to_code_by_solving_problems/3_three_cups.py
@@ -5,6 +5,7 @@
 
 # getting the ip and setting the initial values
 swap_order = input()
+
 position = 1
 
 # looping through the swap order and classifying the outcome
@@ -41,3 +42,27 @@ def swap_cups_trick(swap_order, position):
         if  position == 3 and swap == 'C':
             position=1
     return position
+
+# checking if the input is a string and one of abc
+def swap_cups_trick(swap_order, position):
+    # validation
+    if not isinstance(swap_order, str) or not set(swap_order).issubset({'A', 'B', 'C'}):
+        print("Please provide a valid string containing only 'A', 'B', and 'C'.")
+        return None
+    
+    # conditional classification to find final position
+    else:
+        for swap in swap_order:
+            if position == 1 and swap == 'A':
+                position=2
+            if position == 1 and swap == 'C':
+                position=3
+            if  position == 2 and swap == 'A':
+                position=1
+            if  position == 2 and swap == 'B':
+                position=3
+            if  position == 3 and swap == 'B':
+                position=2
+            if  position == 3 and swap == 'C':
+                position=1
+        return position
