4a0e8f46c351886011f91a0f800c0b790b8ed62a
srikal <srikaldata@gmail.com>
2026-07-10 20:44:10 -0400

    learn to code by solving problems - occupied parking spaces - getting the inputs

---
+++ learning_to_code_by_solving_problems/4_occupied_spaces.py
@@ -0,0 +1,10 @@
+# number of parking spaces that were occupied on yesterday and today
+
+# inputs:
+# num of parking spaces
+# yesterday pattern with . means empty spot, C means the car has occupied the space
+# today's num of parking spots using . means empty spot, C means the car has occupied the space
+
+# getting the inputs
+num_spaces = int(input("num of spaces: "))
+parking_pattern_both_days = input("today's parking pattern: ") + input("yesterday's parking pattern: ")
