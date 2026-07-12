156985f11c775241be25517e124f2b2fbbcfb6ce
srikal <srikaldata@gmail.com>
2026-07-11 23:28:03 -0400

    learn to code by solving problems - data plan - setting the problem inputs and outputs

---
+++ learning_to_code_by_solving_problems/5_data_plan.py
@@ -0,0 +1,9 @@
+# data plan with monthly carryover. find total data available currently
+
+# inputs: 
+# x --> data in MB per month
+# n --> number of months 
+# set of lines with data usage per month in MB for each of the n months
+
+# output:
+# data in MB remaining and available for next month
