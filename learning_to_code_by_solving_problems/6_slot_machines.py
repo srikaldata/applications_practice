307cb07e09db9150de8faaf5f4cadd34269bcccd
srikal <srikaldata@gmail.com>
2026-07-13 21:13:28 -0400

    learn to code by solving problems - slot machines - getting the inputs

---
+++ learning_to_code_by_solving_problems/6_slot_machines.py
@@ -0,0 +1,17 @@
+# how many times to play the slot machine before going broke
+
+# input:
+# num of quarters available
+# num times the first slot machine has already played
+# num times the second slot machine already played
+# num times the third slot machine already played
+
+# output:
+# total tries before going broke
+
+num_quarters = int(input('num of quarters available: '))
+first_m_plays = int(input('num of previous plays in FIRST machine: '))
+second_m_plays = int(input('num of previous plays in SECOND machine: '))
+third_m_plays = int(input('num of previous plays in THIRD machine: '))
+
+# 
