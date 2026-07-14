31a86940efa649fc0b93b115da2e4067d7fdf147
srikal <srikaldata@gmail.com>
2026-07-13 21:26:32 -0400

    learn to code by solving problems - slot machines - setting the logic for the first machine

--- learning_to_code_by_solving_problems/6_slot_machines.py
+++ learning_to_code_by_solving_problems/6_slot_machines.py
@@ -14,4 +14,16 @@ first_m_plays = int(input('num of previous plays in FIRST machine: '))
 second_m_plays = int(input('num of previous plays in SECOND machine: '))
 third_m_plays = int(input('num of previous plays in THIRD machine: '))
 
-# 
+machine = 1
+num_plays = 0
+
+# looping through playing each machine for each quarter in a continuous loop until the person is broke
+while num_quarters >= 1:
+    num_quarters -= 1
+    
+    # when playing in the first machine:
+    if machine == 1:
+        first_m_plays +=1
+        if first_m_plays == 35:
+            num_quarters += 30
+            first_m_plays=0
