a52c28d0756ea85f8d0a3143725fd1691ff24735
srikal <srikaldata@gmail.com>
2026-07-15 19:27:39 -0400

    learn to code by solving problems - secret sentence - initiating the result and index

---
+++ learning_to_code_by_solving_problems/8_secret_sentence.py
@@ -0,0 +1,14 @@
+# decoding secret coded sentence
+
+# input:
+# encoded sentence with the letter p and the same vowel when a vowel comes up
+
+# output:
+# original sentence decoded
+
+sentence = 'ipi lipikepe yopoupu'
+
+# initiating the result var and character index
+result = ''
+char_idx = 0
+
