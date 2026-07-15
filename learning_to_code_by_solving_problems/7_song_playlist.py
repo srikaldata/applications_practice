d3afd6e6e3e64e1c11193df137c13b898b974346
srikal <srikaldata@gmail.com>
2026-07-14 20:36:26 -0400

    learn to code by solving problems - song playlist - setting the initial settings for a playlist

---
+++ learning_to_code_by_solving_problems/7_song_playlist.py
@@ -0,0 +1,12 @@
+# song playlist order change
+
+# button 1 - move first song to the end
+# button 2 - more last song to the start
+# button 3 - swap the first and second songs
+# button 4 - plays the playlist 
+
+# initial song order
+songs_order = 'ABCDE'
+
+# initializing button value
+button =0
