0d02596c36aa14c3e2a7f534c716513bafe12378
srikal <srikaldata@gmail.com>
2026-07-16 23:41:30 -0400

    learn to code by solving problems - village neighborhood - getting the inputs

---
+++ learning_to_code_by_solving_problems/9_village_neighborhood.py
@@ -0,0 +1,15 @@
+# find the size of the smallest neghborhood
+
+# input
+# num of villages 3 to 100
+# village positions for all subsequent inputs
+
+# output
+# size of the smallext neighborhood with 1 digit after decimal pt
+
+num_villages = int(input('num of villages:'))
+village_positions = []
+
+for _ in range(num_villages):
+    village_positions.append(int(input('position of the village:')))
+
