7e1b6d122a47a44c395ee34da026161725ab19e5
srikal <srikaldata@gmail.com>
2026-07-16 23:50:01 -0400

    learn to code by solving problems - village neighborhood - constructing a function to calculate the minimum village size

--- learning_to_code_by_solving_problems/9_village_neighborhood.py
+++ learning_to_code_by_solving_problems/9_village_neighborhood.py
@@ -31,3 +31,22 @@ for i in range(1, num_villages-1):
         min_village_size=village_size
 
 print(min_village_size)
+
+# wrapping up the logic within a function
+def calc_min_village_size(num_villages, village_positions):
+    # initializing village size
+    min_village_size = float(1000000000)
+
+    # calculating the minimum size of each village
+    for i in range(1, num_villages-1):
+        left_limit = (village_positions[i]-village_positions[i-1])/2
+        right_limit= (village_positions[i+1]-village_positions[i])/2
+        
+        village_size = left_limit + right_limit
+        
+        # updating previous minimum village size
+        if village_size < min_village_size:
+            min_village_size=village_size
+
+    print(min_village_size)
+    
