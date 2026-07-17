260e23efb35b12fc47c2e74aec8c79ff0d32f5f2
srikal <srikaldata@gmail.com>
2026-07-17 09:18:16 -0400

    learn to code by solving problems - school trip - setting the constant rate per student of each year

---
+++ learning_to_code_by_solving_problems/10_school_trip.py
@@ -0,0 +1,13 @@
+# school trip brunch based funding
+
+# inputs:
+# cost in dollars of school trip
+# proportion of students at brunch 1st year, 2nd year, 3rd year, 4th year
+# num of students attending the brunch
+
+# output:
+# if students need to raise money --> YES. if not --> NO
+
+# setting the constant
+cost_per_student_diff_yrs = [12, 10, 7, 5]
+
