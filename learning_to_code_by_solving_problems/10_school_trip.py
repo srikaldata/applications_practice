d621558868d829ae363f9b1ff309c2e6fef46ded
srikal <srikaldata@gmail.com>
2026-07-17 09:33:53 -0400

    learn to code by solving problems - school trip - calculating total amount raised from brunch

--- learning_to_code_by_solving_problems/10_school_trip.py
+++ learning_to_code_by_solving_problems/10_school_trip.py
@@ -14,7 +14,7 @@ cost_per_student_diff_yrs = [12, 10, 7, 5]
 # getting the inputs
 for _ in range(10):
     trip_cost = int(input('cost of the trip in $: '))
-    student_proportions_yrs = input('student proportions in each of 4 years (separate each using a space): ').split()
+    student_proportions_yrs = input('student proportions in each of 4 years (separate each using a space) (MUST TOTAL TO 1.0): ').split()
     num_students=int(input('number of students: '))
     
     # converting list of strings to list of floats
@@ -24,9 +24,16 @@ for _ in range(10):
     print(trip_cost, student_proportions_yrs, num_students, sep='\n\n')
     
     # calculating the num of students in each year using proportion
-    students_yrs = []
+    students_each_yr = []
     
     for prop in student_proportions_yrs:
-        students_yrs.append(int(prop*num_students))
+        students_each_yr.append(int(prop*num_students))
         
+    # calculating the total amount raised from brunch 
+    total_amt_raised = 0
+    for idx in range(len(students_each_yr)):
+        total_amt_raised= total_amt_raised + (students_each_yr[idx], cost_per_student_diff_yrs[idx])
+        
+    print('total amount raised: ', total_amt_raised)
+    
 
