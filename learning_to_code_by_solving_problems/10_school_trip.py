92ce86e1e965d697ad821851894c9d5cf1b74f99
srikal <srikaldata@gmail.com>
2026-07-17 09:23:17 -0400

    learn to code by solving problems - school trip - getting the inputs of total trip cost proportion of students and total number of students

--- learning_to_code_by_solving_problems/10_school_trip.py
+++ learning_to_code_by_solving_problems/10_school_trip.py
@@ -11,3 +11,11 @@
 # setting the constant
 cost_per_student_diff_yrs = [12, 10, 7, 5]
 
+# getting the inputs
+for _ in range(10):
+    trip_cost = int(input('cost of the trip in $: '))
+    student_proportions_yrs = input('student proportions in each of 4 years (separate each using a space): ').split()
+    num_students=int(input('number of students: '))
+    
+    print(trip_cost, student_proportions_yrs, num_students, sep='\n\n')
+
